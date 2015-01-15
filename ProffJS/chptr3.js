ae9a9d6f0c4c3d8a4a7a5163230cd8c09fb31941
benstoltz <stoltz.ben@me.com>
2015-01-15 10:36:14 -0500

    chptr3 commit
    Updating chptr 3.
    
    Signed-off-by: benstoltz <stoltz.ben@me.com>

--- ProffJS/chptr3.js
+++ ProffJS/chptr3.js
@@ -65,6 +65,104 @@ if (message) {
 }
 
 
+// Use isNaN() to check whether something is NaN
+
+console.log(isNaN(NaN));
+console.log(isNaN(10)); //False because number
+console.log(isNaN('10')); // False can be converted into a number
+console.log(isNaN("blue")); //True because string/can not be converted into a number
+console.log(isNaN(true)); // False because converts to 1.
+
+
+// can convert values to string with either toString(), String(), or by adding an empty string "".
+var num = 10;
+console.log(num.toString());
+var value1 = 10;
+console.log(String(value1)); // calls the objects toString() method (with no arguments). Returns "null" or "undefined" for those types
+
+console.log(10 + "");
+
+
+// Logical NOT (!)
+
+console.log(!false); //true
+console.log(!"blue"); //false
+console.log(!0); //true
+console.log(!NaN); //true
+console.log(!"");  //true
+console.log(!123456); //false
+
+// Using two ! simulates using the Boolean() function on a value.
+
+console.log(!!false); //false
+console.log(!!"blue"); //true
+console.log(!!0); //false
+console.log(!!NaN); //false
+console.log(!!"");  //false
+console.log(!!123456); //true
+
+
+// logical AND uses && Evaluates true if BOTH operands evaluate to true. If the first operand determines the result the second is never evaluated.
+
+//var found = true;
+//var result = (found && someUndeclaredVariable);
+//console.log(result); // fails because someUndeclaredVariable doesn't exist
+
+var found = false;
+var result = (found && someUndeclaredVariable);
+console.log(result); // returns false and works because found evaluates/determines the result.
+
+
+
+// Logical OR represented by || works similar to AND. If the first operand is True the second never evaluates
+
+// also can be used to store default values
+
+
+//var myObject = preferredObject || backupObject;  // If preferredObject has a value it is stored, however if it is false then the backup object is stored.
+
+
+
+// Conditional operator
+
+var num1 = 132;
+var num2 = 42;
+
+// variabel = boolean_expression ? true_value : false_value
+
+var max = (num1 > num2) ? num1 : num2;
+console.log(max);
+
+
+
+// do-while loop, done when you want the body to execute at least once. Post-test.
+
+
+var i = 0;
+do {
+    i += 2;
+    console.log(i);
+} while (i < 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
