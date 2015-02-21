d43dfbd16557474f7d71c7c7cb22002c97eec515
benstoltz <stoltz.ben@me.com>
2015-02-21 12:11:15 -0500

    animal object
    Animal object added, and created some entries
    
    Signed-off-by: benstoltz <stoltz.ben@me.com>

--- frontEndMasters/js_from_fund_to_functional/JS102-master/scripts.js
+++ frontEndMasters/js_from_fund_to_functional/JS102-master/scripts.js
@@ -1,3 +1,67 @@
 //Use this file to implement Part One of your project
 
+var animal = {};
 
+animal.username = "cow";
+
+animal['tagline'] = 'Yippeeee!';
+
+var noiseArray = ['cough'];
+
+noiseArray.push("yell");
+noiseArray.unshift("whisper");
+
+noiseArray[3] = 'scream';
+
+
+var noises = noiseArray;
+
+animal['noises'] = noises;
+
+for (var key in animal) {
+    if (key === 'username') {
+        console.log("Hi my name is " + animal[key]);
+    } else if (key === 'tagline') {
+        console.log("I like to say " + animal[key]);
+    }
+}
+
+
+var animals = [];
+
+animals.push(animal);
+
+var quackers = {
+    username: 'DaffyDuck',
+    tagline: 'Yippeeeee!',
+    noises: [
+        'quack',
+        'honk',
+        'sneeze',
+        'growl'
+    ]
+};
+
+animals.unshift(quackers);
+
+animals[2] = {
+    username: 'Sheep',
+    tagline: 'Oh god no',
+    noises: [
+        'scream',
+        'cavort',
+        'giggle'
+    ]
+};
+
+var pirahna = {
+    username: 'pirahna',
+    tagline: 'Nom nom nom',
+    noises: [
+        'scream',
+        'nom nom nom nom',
+        'nom nom nom nom nom nom',
+        'swish'
+    ]
+};
+animals.push(pirahna);
