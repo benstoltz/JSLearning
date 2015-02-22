dd46e6d3001894fc4297b6e260a35ec99a11904a
benstoltz <stoltz.ben@me.com>
2015-02-22 11:49:14 -0500

    Functions added
    Function constructors added
    
    Signed-off-by: benstoltz <stoltz.ben@me.com>

--- frontEndMasters/js_from_fund_to_functional/JS102-master/scripts.js
+++ frontEndMasters/js_from_fund_to_functional/JS102-master/scripts.js
@@ -65,3 +65,80 @@ var pirahna = {
     ]
 };
 animals.push(pirahna);
+
+function AnimalMaker(name) {
+    return {
+        speak: function () {
+            console.log("My name is ", name);
+        },
+        name: name,
+        owner: 'Ben'
+    };
+}
+
+var animalNames = ['Sheep', 'Liger', 'Big Bird'];
+var farm = [];
+for (var i = 0; i < animalNames.length + 1; i++) {
+    farm.push(AnimalMaker(animalNames[i]));
+}
+
+for ( var key in farm ) {
+    farm[key].speak();
+}
+
+function AnimalTestUser(username) {
+    var args = arguments.length;
+    var otherArgs = [];
+    if (args > 1) {
+        for (var i = 1; i < args; i++) {
+            otherArgs.push(arguments[i]);
+        }
+    }
+    return {
+        username: username,
+        otherArgs: otherArgs
+    };
+}
+
+function AnimalCreator(username, species, tagline, noises) {
+    return {
+        username: username,
+        species: species,
+        tagline: tagline,
+        noises: noises,
+        friends: []
+    };
+}
+
+var sheep = AnimalCreator('Cloud', 'sheep', 'You can count on me!', ['bah', 'woof', 'whaaaaa?']);
+console.log(sheep);
+
+
+function addFriend(animal, friend) {
+    animal.friends.push(friend.username);
+}
+
+myFarm = [];
+
+function addMatchesArray(farm) {
+    for (var animal in farm) {
+        farm[animal].matches = [];
+    }
+}
+
+function giveMatches(farm) {
+    for (var animal in farm) {
+        farm[animal].matches.push(farm[animal].friends[0]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
